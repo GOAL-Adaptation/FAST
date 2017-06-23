dd1a1a7c8a068f028c9fc29fe054b50b01c9be7e
Ferenc A Bartha <ferinho@gmail.com>
2017-06-23 14:27:26 -0700

    Add JSON converter

---
+++ Sources/FAST/Utils/JSON.swift
@@ -0,0 +1,43 @@
+    /*
+ *  FAST: An implicit programing language based on SWIFT
+ *
+ *        JSON Conversions
+ *
+ *  author: Ferenc A Bartha
+ *
+ *  SWIFT implementation is based on the C library [pemu] implemented by
+ *  Ferenc A Bartha, Dung X Nguyen, Jason Miller, Adam Duracz
+ *
+ *  Core functionalities implemented in this file are based on 
+ *  the example provided by IBM for BlueSocket
+ */
+
+//-------------------------------
+
+import Foundation
+
+//-------------------------------
+
+// TODO status is Dictionary [String : Any], the new custom types eventhough extend String are not recognized as Strings by the 
+// JSONSerializer (See Utils/JSON.swift), hence a JSON object cannot be created automatically. Find a way to hack it or use another library or custom implementation
+
+/** SWIFT built-in conversion to JSON */    
+func convertToJson(from rawData: Any) -> Data? {
+
+    // Recursion on rawData, every leaf has to be recognized as of valid type
+    if JSONSerialization.isValidJSONObject(rawData) {
+        do {
+
+            return try JSONSerialization.data(withJSONObject: rawData, options: .prettyPrinted)
+
+        } catch {
+
+            return nil
+        }
+    } else {
+
+        return nil
+    }
+}
+
+//-------------------------------
