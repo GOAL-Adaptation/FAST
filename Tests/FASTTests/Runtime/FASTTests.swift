785b0d71399a882ab9e6d0943fcb3cf0ccd150ce
Adam Duracz <adam@duracz.net>
2017-07-19 18:05:15 -0500

    Add quick check of absence of SR-221.
    This closes PRO-145.

--- Tests/FASTTests/Runtime/FASTTests.swift
+++ Tests/FASTTests/Runtime/FASTTests.swift
@@ -43,5 +43,28 @@ class FASTTests: XCTestCase {
 
     }
 
+    /**
+     * Quick check to see if https://bugs.swift.org/browse/SR-221
+     * affects the compiler that executes this test suite.
+     * The test code corresponds to that of the first of the
+     * 'Generated Superclass Mirrors' in:
+     *    https://github.com/apple/swift/blob/master/test/stdlib/Mirror.swift#L235
+     */
+    func testSR221() {
+        class B : CustomReflectable {
+            var b: String = "two"
+            var customMirror: Mirror {
+            return Mirror(
+                self, children: ["bee": b], ancestorRepresentation: .generated)
+            }
+        }
+        
+        let b = Mirror(reflecting: B())
+        XCTAssertTrue(b.subjectType == B.self)
+        XCTAssertNil(b.superclassMirror)
+        XCTAssertEqual(1, b.children.count)
+        XCTAssertEqual("bee", b.children.first!.label)
+        XCTAssertEqual("two", b.children.first!.value as? String)
+    }
 }
 
