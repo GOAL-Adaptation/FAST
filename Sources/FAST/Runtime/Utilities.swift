12457b34c1c6551278fa9625e1604d555430768e
Adam Duracz <adam@duracz.net>
2017-07-23 22:16:05 -0700

    Improve error reporting.

--- Sources/FAST/Runtime/Utilities.swift
+++ Sources/FAST/Runtime/Utilities.swift
@@ -9,19 +9,21 @@ func synchronized<L: NSLocking>(_ lock: L, routine: () -> ()) {
 
 func readFile( withName name: String, ofType type: String
              , fromBundle bundle: Bundle = Bundle.main ) -> String? {
+
     if let path = bundle.path(forResource: name, ofType: type) {       
         do {
             let contents = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
-            Log.debug("Loaded file '\(name).\(type)'.")
-            return contents            
+            Log.debug("Loaded file '\(path)'.")
+            return contents
         }
         catch let error {
-            Log.warning("Unable to load file '\(name).\(type)'. \(error)")
+            Log.warning("Unable to load file '\(path)'. \(error)")
             return nil
         }
     }
-    else {
-        Log.warning("Unable to load file '\(name).\(type)'.")
+    else {        
+        Log.warning("No file '\(name).\(type)' in \(Bundle.main).")
         return nil
     }
-} 
+
+}
