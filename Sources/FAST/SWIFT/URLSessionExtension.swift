4dfa5c08a513392df1e819ffe33e3da5160034b4
Adam Duracz <adam@duracz.net>
2017-09-26 11:25:57 -0500

    Add missing extension.

---
+++ Sources/FAST/SWIFT/URLSessionExtension.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+extension URLSession {
+    
+    func synchronousDataTask(urlRequest: URLRequest) -> (Data?, URLResponse?, Error?) {
+    
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let dataTask = self.dataTask(with: urlRequest) {
+            data = $0
+            response = $1
+            error = $2
+
+            semaphore.signal()
+        }
+        dataTask.resume()
+
+        _ = semaphore.wait(timeout: .distantFuture)
+
+        return (data, response, error)
+
+    }
+}
