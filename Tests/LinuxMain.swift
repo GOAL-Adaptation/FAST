beb2fc6933a2079d5b3cff4190ed13f4eb0b481d
Adam Duracz <adam@duracz.net>
2017-10-13 14:27:13 -0500

    Add all test suites to LinuxMain.
    So that they are included when running `make test` on Linux.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,9 @@ import XCTest
 @testable import FASTTests
 
 XCTMain([
+    testCase(CompilerTests.allTests),
     testCase(FASTTests.allTests),
+    testCase(IntentTests.allTests),
+    testCase(OptimizeTests.allTests),
+    testCase(RestServerTests.allTests)
 ])
