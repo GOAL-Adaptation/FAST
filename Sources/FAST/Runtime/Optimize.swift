277fe48a0362a96d5813fe2b4b0bdab1e2771320
Adam Duracz <adam@duracz.net>
2017-10-16 17:08:42 -0500

    Accept JSON missionIntent in Perturbation initializer.

--- Sources/FAST/Runtime/Optimize.swift
+++ Sources/FAST/Runtime/Optimize.swift
@@ -75,18 +75,41 @@ struct Perturbation {
 
     init?(json: [String: Any]) {
         
-        if let missionIntentString    = extract(type: String.self, name: "missionIntent"         , json: json)
-         , let missionIntent          = compiler.compileIntentSpec(source: missionIntentString)
-         , let availableCores         = extract(type: UInt16.self, name: "availableCores"        , json: json)
+        if let availableCores         = extract(type: UInt16.self, name: "availableCores"        , json: json)
          , let availableCoreFrequency = extract(type: UInt64.self, name: "availableCoreFrequency", json: json)
          , let missionLength          = extract(type: UInt64.self, name: "missionLength"         , json: json)
          , let sceneObfuscation       = extract(type: Double.self, name: "sceneObfuscation"      , json: json) {
 
-            self.missionIntent          = missionIntent
             self.availableCores         = availableCores
             self.availableCoreFrequency = availableCoreFrequency
             self.missionLength          = missionLength
             self.sceneObfuscation       = sceneObfuscation
+
+            if let missionIntentString = json["missionIntent"] as? String {
+                if let missionIntent = compiler.compileIntentSpec(source: missionIntentString) {
+                    self.missionIntent = missionIntent
+                }
+                else {
+                    Log.error("Unable to compile missionIntent from string: \(missionIntentString), which is part of the perturbation JSON: \(json).")
+                    return nil   
+                }
+            }
+            else {
+                if let missionIntentJson = json["missionIntent"] as? [String : Any] {
+                    let missionIntentString = RestServer.mkIntentString(from: json)
+                    if let missionIntent = compiler.compileIntentSpec(source: missionIntentString) {
+                        self.missionIntent = missionIntent
+                    }
+                    else {
+                        Log.error("Unable to compile missionIntent from string: \(missionIntentString), obtained from missionIntent JSON: \(missionIntentJson), which is part of the perturbation JSON: \(json).")
+                        return nil   
+                    }
+                }
+                else {
+                    Log.error("Unable to parse missionIntent from JSON: \(String(describing: json["missionIntent"])), which is part of the perturbation JSON: \(json).")
+                    return nil   
+                }
+            }
 
         }
         else {
