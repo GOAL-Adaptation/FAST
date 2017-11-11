1d5f1a4d2d280e965dcaa2c67f45c8a8c04ad049
Adam Duracz <adam@duracz.net>
2017-11-11 09:28:54 -0600

    Add initializer that takes the fixed configuration as a parameter.

--- Sources/FAST/Runtime/Controller.swift
+++ Sources/FAST/Runtime/Controller.swift
@@ -30,11 +30,21 @@ class ConstantController : Controller {
 
     let window: UInt32 = 1
 
+    private let knobSettings: KnobSettings
+
+    init(knobSettings: KnobSettings) {
+        self.knobSettings = knobSettings
+    }
+
+    init() {
+        // FIXME Eliminate undefined-value representations (-1 and [:]) below
+        //       by making the Runtime.controller optional.
+        self.knobSettings = KnobSettings(kid: -1, [:]) 
+    }
+
     func getSchedule(_ intent: IntentSpec, _ measureValues: [String : Double]) -> Schedule {
         return Schedule({ (_: UInt32) -> KnobSettings in 
-            // FIXME Eliminate undefined-value representations (-1 and [:]) below
-            //       by making the Runtime.controller optional.
-            return KnobSettings(kid: -1, [:]) 
+            return self.knobSettings
         })
     }
 
