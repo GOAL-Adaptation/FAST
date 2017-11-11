23b6d02f70404a0410169904fd7870d36d21d464
Adam Duracz <adam@duracz.net>
2017-11-11 09:26:42 -0600

    Make parseKnobSetting function accept Any.

--- Sources/FAST/Runtime/Model.swift
+++ Sources/FAST/Runtime/Model.swift
@@ -20,13 +20,30 @@ import LoggerAPI
 
 //---------------------------------------
 
-func parseKnobSetting(setting: String) -> Any {
+func parseKnobSetting(setting: Any) -> Any {
     // TODO Add support for other knob types, based on type information in intent spec, and error handling
-    if let i = Int(setting) {
-        return i
-    } 
-    if let d = Double(setting) {
-        return d
+    if let s = setting as? String {
+        if let i = Int(s) {
+            return i
+        } 
+        else {
+            if let d = Double(s) {
+                return d
+            }
+            else {
+                Log.error("Could not parse knob setting \(setting) of type \(type(of: setting)).")
+                fatalError()
+            }
+        }
+    }
+    else {
+        if setting is Double || setting is Int {
+            return setting
+        }
+        else {
+            Log.error("Could not parse knob setting \(setting) of type \(type(of: setting)).")
+            fatalError()
+        }
     }
     return setting
 }
