d6c5e689f1536298aabdd713f6110faee4b43a17
Ryuichi Sai <ryuichi@ryuichisai.com>
2017-12-28 14:18:10 -0600

    Runtime models can exist outside __Runtime namespace

--- Sources/FAST/Runtime/RuntimeModels/RuntimeApiModule.swift
+++ Sources/FAST/Runtime/RuntimeModels/RuntimeApiModule.swift
@@ -1,63 +1,61 @@
-extension __Runtime {
-    // The runtime API this where the channel connects
-    public class RuntimeApiModule: TextApiModule {
-        public let name = "Runtime"
-        public var subModules = [String : TextApiModule]()
+// The runtime API this where the channel connects
+public class RuntimeApiModule: TextApiModule {
+    public let name = "Runtime"
+    public var subModules = [String : TextApiModule]()
 
-        public func internalTextApi(
-            caller: String,
-            message: [String],
-            progressIndicator: Int,
-            verbosityLevel: VerbosityLevel
-        ) -> String {
-            func consRet(_ verboseText: String, _ defaultText: String = "") -> String {
-                guard case .Verbose = verbosityLevel else { return defaultText }
-                return verboseText
-            }
+    public func internalTextApi(
+        caller: String,
+        message: [String],
+        progressIndicator: Int,
+        verbosityLevel: VerbosityLevel
+    ) -> String {
+        func consRet(_ verboseText: String, _ defaultText: String = "") -> String {
+            guard case .Verbose = verbosityLevel else { return defaultText }
+            return verboseText
+        }
 
-            // the internal runtime API handles the process command
-            if message[progressIndicator] == "process" {
-                if Runtime.runtimeKnobs.interactionMode.get() == .Scripted {
-                    var stepAmount: UInt64 = 0
-                    if message[progressIndicator + 1] == "random" {
-                        if message.count > progressIndicator + 3 {
-                            if  let loBound = Int32(message[progressIndicator + 2]),
-                                let hiBound = Int32(message[progressIndicator + 3]) {
-                                    stepAmount = UInt64(randi(min: loBound, max: hiBound))
-                            }
+        // the internal runtime API handles the process command
+        if message[progressIndicator] == "process" {
+            if Runtime.runtimeKnobs.interactionMode.get() == .Scripted {
+                var stepAmount: UInt64 = 0
+                if message[progressIndicator + 1] == "random" {
+                    if message.count > progressIndicator + 3 {
+                        if  let loBound = Int32(message[progressIndicator + 2]),
+                            let hiBound = Int32(message[progressIndicator + 3]) {
+                                stepAmount = UInt64(randi(min: loBound, max: hiBound))
                         }
-                    } else if let stepNumber = UInt64(message[progressIndicator + 1]) {
-                        stepAmount = stepNumber
                     }
-
-                    if stepAmount > 0 {
-                        Runtime.process(numberOfInputs: stepAmount)
-                        return consRet("Processed \(stepAmount) input(s).")
-                    } else {
-                        return consRet("Invalid step amount for process: ``" + message.joined(separator: " ") + "`` received from: \(caller).")
-                    }
-                } else {
-                    return consRet("Invalid process message: ``" + message.joined(separator: " ") + "`` received from: \(caller) as interactionMode is \(Runtime.runtimeKnobs.interactionMode.get()).")
+                } else if let stepNumber = UInt64(message[progressIndicator + 1]) {
+                    stepAmount = stepNumber
                 }
-            // the runtime keeps track of the iteration measure
-            } else if message[progressIndicator] == "iteration" && message[progressIndicator + 1] == "get" {
-                return consRet(
-                    "Current iteration is: " + String(describing: Runtime.getMeasure("iteration")) + ".",
-                    String(describing: Runtime.getMeasure("iteration"))
-                )
-            // invalid message
+
+                if stepAmount > 0 {
+                    Runtime.process(numberOfInputs: stepAmount)
+                    return consRet("Processed \(stepAmount) input(s).")
+                } else {
+                    return consRet("Invalid step amount for process: ``" + message.joined(separator: " ") + "`` received from: \(caller).")
+                }
             } else {
-                return consRet("Invalid message: ``" + message.joined(separator: " ") + "'' received from: \(caller).")
+                return consRet("Invalid process message: ``" + message.joined(separator: " ") + "`` received from: \(caller) as interactionMode is \(Runtime.runtimeKnobs.interactionMode.get()).")
             }
+        // the runtime keeps track of the iteration measure
+        } else if message[progressIndicator] == "iteration" && message[progressIndicator + 1] == "get" {
+            return consRet(
+                "Current iteration is: " + String(describing: Runtime.getMeasure("iteration")) + ".",
+                String(describing: Runtime.getMeasure("iteration"))
+            )
+        // invalid message
+        } else {
+            return consRet("Invalid message: ``" + message.joined(separator: " ") + "'' received from: \(caller).")
         }
+    }
 
-        /** get status as a dictionary */
-        public func getInternalStatus() -> [String : Any]? {
-            return ["iteration" : UInt64(Runtime.getMeasure("iteration")!)] // TODO make sure iteration is always defined, some global init would be nice
-        }
+    /** get status as a dictionary */
+    public func getInternalStatus() -> [String : Any]? {
+        return ["iteration" : UInt64(Runtime.getMeasure("iteration")!)] // TODO make sure iteration is always defined, some global init would be nice
+    }
 
-        init() {
-            self.addSubModule(newModule: Runtime.runtimeKnobs)
-        }
+    init() {
+        self.addSubModule(newModule: Runtime.runtimeKnobs)
     }
 }
--- Sources/FAST/Runtime/RuntimeModels/RuntimeKnobs.swift
+++ Sources/FAST/Runtime/RuntimeModels/RuntimeKnobs.swift
@@ -1,19 +1,17 @@
-extension __Runtime {
-    // These knobs control the interaction mode (e.g. scripted) and application execution mode (e.g. profiling)
-    class RuntimeKnobs: TextApiModule {
-        let name = "RuntimeKnobs"
+// These knobs control the interaction mode (e.g. scripted) and application execution mode (e.g. profiling)
+class RuntimeKnobs: TextApiModule {
+    let name = "RuntimeKnobs"
 
-        var subModules = [String : TextApiModule]()
+    var subModules = [String : TextApiModule]()
 
-        var interactionMode: Knob<InteractionMode>
+    var interactionMode: Knob<InteractionMode>
 
-        var applicationExecutionMode: Knob<ApplicationExecutionMode>
+    var applicationExecutionMode: Knob<ApplicationExecutionMode>
 
-        init(_ key: [String]) {
-            self.interactionMode = Knob(name: "interactionMode", from: key, or: InteractionMode.Default, preSetter: Runtime.changeInteractionMode)
-            self.applicationExecutionMode = Knob(name: "applicationExecutionMode", from: key, or: ApplicationExecutionMode.Adaptive)
-            self.addSubModule(newModule: interactionMode)
-            self.addSubModule(newModule: applicationExecutionMode)
-        }
+    init(_ key: [String]) {
+        self.interactionMode = Knob(name: "interactionMode", from: key, or: InteractionMode.Default, preSetter: Runtime.changeInteractionMode)
+        self.applicationExecutionMode = Knob(name: "applicationExecutionMode", from: key, or: ApplicationExecutionMode.Adaptive)
+        self.addSubModule(newModule: interactionMode)
+        self.addSubModule(newModule: applicationExecutionMode)
     }
 }
