37f53c6593cd118bc40fa33c9b45228dc7cbb925
Adam Duracz <adam@duracz.net>
2018-01-03 13:45:36 -0600

    Switch back to upstream Perfect-SQLite
    Since we no longer use the custom extension to compute STDEV.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
       .Package(url: "https://github.com/Daniel1of1/CSwiftV", majorVersion: 0),
       .Package(url: "git@github.mit.edu:proteus/CEnergymon", majorVersion: 1),
       .Package(url: "git@github.mit.edu:proteus/FASTController", majorVersion: 1),
-      .Package(url: "https://github.com/barfer/Perfect-SQLite.git", majorVersion: 2),
+      .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2),
       .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
       .Package(url: "https://github.com/jasonm128/Perfect-HTTPServer", majorVersion: 99),
       .Package(url: "https://github.com/ryuichis/swift-ast", majorVersion: 0)
