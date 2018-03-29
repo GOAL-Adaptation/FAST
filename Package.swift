81eb69d0083a8838c5f7ab074778d0d9a6cc4392
Ryuichi Sai <ryuichi@ryuichisai.com>
2018-03-29 18:55:17 -0500

    Update to Swift Package Manager 4 format with revised dependencies

--- Package.swift
+++ Package.swift
@@ -1,20 +1,28 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "FAST",
-    targets: [
-      Target(name: "FAST", dependencies: []),
-      Target(name: "incrementer", dependencies: ["FAST"])
-    ],
-    dependencies: [
-      .Package(url: "https://github.com/Zewo/Venice", majorVersion: 0, minor: 14),
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 7),
-      .Package(url: "https://github.com/Daniel1of1/CSwiftV", majorVersion: 0),
-      .Package(url: "git@github.mit.edu:proteus/CEnergymon", majorVersion: 1),
-      .Package(url: "git@github.mit.edu:proteus/FASTController", majorVersion: 1),
-      .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
-      .Package(url: "https://github.com/jasonm128/Perfect-HTTPServer", majorVersion: 99),
-      .Package(url: "https://github.com/ryuichis/swift-ast", majorVersion: 0)
-    ]
+  name: "FAST",
+  products: [
+    .executable(name: "incrementer", targets: ["incrementer"]),
+    .library(name: "FAST", targets: ["FAST"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Zewo/Venice", .exact("0.20.0")),
+    .package(url: "https://github.com/IBM-Swift/HeliumLogger", .exact("1.7.1")),
+    .package(url: "https://github.com/Daniel1of1/CSwiftV", .exact("0.0.7")),
+    .package(url: "git@github.mit.edu:proteus/CEnergymon", .exact("1.0.1")),
+    .package(url: "git@github.mit.edu:proteus/FASTController", .exact("1.0.2")),
+    .package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", .exact("3.0.1")),
+    .package(url: "https://github.com/IBM-Swift/CCurl", .exact("1.0.0")),
+    .package(url: "https://github.com/jasonm128/Perfect-HTTPServer", .exact("99.0.2")),
+    .package(url: "https://github.com/ryuichis/swift-ast", .revision("06c530a196ce8ef55fc03ecfa542ac7652cbc440")),
+  ],
+  targets: [
+    .target(name: "FAST", dependencies: [
+      "HeliumLogger", "PerfectHTTPServer", "PerfectSQLite", "SwiftAST", "FASTController", "CSwiftV", "Venice"]),
+    .target(name: "incrementer", dependencies: ["FAST"])
+  ],
+  swiftLanguageVersions: [4]
 )
