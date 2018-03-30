913ae7250ee7b9ce7b8749a5f88af8d3522c663f
Ryuichi Sai <ryuichi@ryuichisai.com>
2018-03-30 13:07:26 -0500

    Update Yanagiba

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     .package(url: "git@github.mit.edu:proteus/FASTController", .exact("1.0.3")),
     .package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", .exact("3.0.1")),
     .package(url: "https://github.com/jasonm128/Perfect-HTTPServer", .exact("99.0.2")),
-    .package(url: "https://github.com/ryuichis/swift-ast", .revision("06c530a196ce8ef55fc03ecfa542ac7652cbc440")),
+    .package(url: "https://github.com/ryuichis/swift-ast", .exact("0.2.0")),
   ],
   targets: [
     .target(name: "FAST", dependencies: [
