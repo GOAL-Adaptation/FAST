82aa2f9bea81b67dc7e1ee1ef285c8aeaf212712
Dung Nguyen <dxnguyen@rice.edu>
2018-10-24 14:48:51 -0500

    Change back from git.isis.vanderbilt.edu to github.mit.edu

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/IBM-Swift/HeliumLogger", .exact("1.7.1")),
     .package(url: "https://github.com/Daniel1of1/CSwiftV", .exact("0.0.7")),
-    .package(url: "git@git.isis.vanderbilt.edu:proteus/CEnergymon", .exact("1.0.1")),
-    .package(url: "git@git.isis.vanderbilt.edu:proteus/FASTController", .exact("1.0.3")),
+    .package(url: "git@github.mit.edu:proteus/CEnergymon", .exact("1.0.1")),
+    .package(url: "git@github.mit.edu:proteus/FASTController", .exact("1.0.3")),
     .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer", .exact("3.0.10")),
-    .package(url: "git@git.isis.vanderbilt.edu:proteus/swift-ast", .exact("0.2.0")),
+    .package(url: "git@github.mit.edu:proteus/swift-ast", .exact("0.2.0")),
   ],
   targets: [
     .target(name: "FAST", dependencies: [
