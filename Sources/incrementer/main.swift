9d215ef1da998681f9bb20e63c307d1efa4f7bfc
Dung Nguyen <dxnguyen@rice.edu>
2019-02-21 18:43:25 -0600

    Updated to using new syntax for knob declaration.
    Support for String type for knobs illustrated by knob `step`.

--- Sources/incrementer/main.swift
+++ Sources/incrementer/main.swift
@@ -12,16 +12,16 @@ import Foundation
 import FAST
 
 let threshold = Knob("threshold", 1000000)
-let step = Knob("step", 1)
+let step = Knob("step", "1")
 
 var x = 0
 optimize("incrementer", [threshold, step]) {
     var operations = 0.0
     while(x < threshold.get()) {
-        x += step.get()
+        x += Int(step.get())!
         operations += 1
     }
     x = 0
     measure("operations", operations)
-    measure("quality", 1.0 / Double(step.get()))
+    measure("quality", 1.0 / Double(step.get())!)
 }
