17b22daca0e77dcfcaee0deb4ec1991749a13313
Adam Duracz <adam.duracz@rice.edu>
2019-02-27 10:30:35 -0600

    Bump FASTController to 1.0.4.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     .package(url: "https://github.com/IBM-Swift/HeliumLogger", .exact("1.7.1")),
     .package(url: "https://github.com/Daniel1of1/CSwiftV", .exact("0.0.7")),
     .package(url: "git@github.mit.edu:proteus/CEnergymon", .exact("1.0.1")),
-    .package(url: "git@github.mit.edu:proteus/FASTController", .exact("1.0.3")),
+    .package(url: "git@github.mit.edu:proteus/FASTController", .exact("1.0.4")),
     .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer", .exact("3.0.10")),
     .package(url: "git@github.mit.edu:proteus/swift-ast", .exact("0.2.0")),
     .package(url: "git@github.mit.edu:proteus/UnconstrainedOptimizer", .exact("0.0.3")),
