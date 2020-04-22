457c8e3a7ad443b795aa0662da1b1c00cbdc1e8a
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2020-04-22 16:15:54 -0500

    Change dependencies

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/IBM-Swift/HeliumLogger", .exact("1.7.1")),
     .package(url: "https://github.com/Daniel1of1/CSwiftV", .exact("0.0.7")),
-    .package(url: "git@github.mit.edu:proteus/CEnergymon", .exact("1.0.1")),
-    .package(url: "git@github.mit.edu:proteus/FASTController", .exact("1.0.4")),
+    .package(url: "git@github.com:ahsanp/CEnergymon.git", .exact("1.0.1")),
+    .package(url: "git@github.com:ahsanp/FASTController.git", .exact("1.0.4")),
     .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer", .exact("3.0.10")),
     .package(url: "https://github.com/nicklockwood/Expression", .exact("0.12.11")),
-    .package(url: "git@github.mit.edu:proteus/swift-ast", .exact("0.2.0")),
-    .package(url: "git@github.mit.edu:proteus/UnconstrainedOptimizer", .exact("0.0.3")),
-    .package(url: "git@github.mit.edu:proteus/MulticonstrainedOptimizer", .exact("0.0.15")),
+    .package(url: "git@github.com:ahsanp/swift-ast.git", .exact("0.2.0")),
+    .package(url: "git@github.com:ahsanp/UnconstrainedOptimizer.git", .exact("0.0.3")),
+    .package(url: "git@github.com:proteus/MulticonstrainedOptimizer.git", .exact("0.0.15")),
   ],
   targets: [
     .target(name: "FAST", dependencies: [
